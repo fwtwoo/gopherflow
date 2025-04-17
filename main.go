b138332ed2b61bba5760abb888bfa49f6e01487d
Ebbe <ek224ev@student.lnu.se>
2025-04-17 16:41:08 +0200

    added Cobra-CLI generated files

--- main.go
+++ main.go
@@ -1,52 +1,10 @@
+/*
+Copyright © 2025 Ebbe Karlstad
+*/
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
-)
-
-func printWelcome() {
-	// Print welcome message
-	fmt.Println("╔══════════════════════════════════════════════════════════╗")
-	fmt.Println("║                Welcome to GopherFlow!                    ║")
-	fmt.Println("║  AI-generated, industry-standard commit messages.        ║")
-	fmt.Println("╚══════════════════════════════════════════════════════════╝")
-	fmt.Print("\n")
-}
-
-// func printError() {
-// 	// Print API error message
-// 	fmt.Println("❌ Failed to generate commit message.")
-// 	fmt.Print("\n")
-// 	fmt.Println("Possible reasons:\n- No internet connection\n- API rate limit exceeded")
-// 	fmt.Print("\n")
-// }
+import "github.com/ebbekarlstad/gopherflow/cmd"
 
 func main() {
-	// Load .env file
-	env := godotenv.Load()
-
-	// Error handling for .env file
-	if env != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// Get API Key
-	apiKey := os.Getenv("API_KEY")
-
-	// Print welcome message
-	printWelcome()
-
-	// Scanner saves input using Stdin (standard input)
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-
-	// Save input
-	userInput := scanner.Text()
-	fmt.Println("\nYour input:", userInput)
-	fmt.Printf("API Key: %s\n", apiKey)
+	cmd.Execute()
 }
